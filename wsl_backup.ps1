e74bb7165e3baaec121cf2d573213f01563b8cbc
jtmb <james.branco@gmail.com>
2023-02-23 09:38:05 -0500

    initial commit

--- wsl_backup.ps1
+++ wsl_backup.ps1
@@ -1,9 +1,10 @@
-#VARS
-$BackupDir = "G:\backups\WSL"
-$US_InsideOut = 'MM-dd-yyyy'	
-$date = Get-Date -Format $US_InsideOut
-
-cd $BackupDir
+# /////////////////////VARS ///////////////////////
+$BackupDir = "G:\backups\WSL" #set backup location
+$US_InsideOut = 'MM-dd-yyyy' #set time format to var
+$date = Get-Date -Format $US_InsideOut #Set VAR for date using $US_InsideOut format
+# /////////////////////////////////////////////////
 
 #Export WSL (backup)
-wsl --export Ubuntu wsl_ubuntu_backup_$date.tar
+cd $BackupDir
+wsl --export 
+Ubuntu wsl_ubuntu_backup_$date.tar
